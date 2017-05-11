e1a6713138bcc757ff3a8eac7eac4905e44c9e17
xjping0794 <1064022815@qq.com>
2017-05-11 11:43:38 +0800

    this is rdbms interaction

---
+++ export_load_3.sh
@@ -0,0 +1,262 @@
+#!/usr/bin/ksh
+#set -x   启动"-x"选项 要跟踪的程序段
+##############################################################################################
+# creator\time: xiaojp \201403
+# for example : export_load_3.sh -t1 bass15.bass1_02004_20140131 -t2 bass15.bass1_02004_20140131 -s tbs_test -p 2 
+# 说明：   export_load_2.sh -t1 export_tb1  -t2 load_tb1 -s tbs_space -p type
+#                             -t1:需要导出的源表
+#                             -t2:需要装入的目标表
+#                             -s :目标表所处的空间
+#                             -p ： 2为一经导二经 ,1为二经导一经,3为二经导数据集市
+##############################################################################################
+
+
+echo "get command variable......"
+if [ $# -eq 8 ]; then
+  while [ $# -ne 0 ] 
+  do
+        if [ x$1 == 'x-t1' ]; then 
+        shift
+        export_tb1=$1
+        shift 
+        elif [ x$1 == 'x-t2' ]; then
+        shift
+        load_tb1=$1
+        shift 
+        elif [ x$1 == 'x-s' ]; then
+        shift
+        tbs_space=$1
+        shift
+        elif [ x$1 == 'x-p' ]; then
+        shift
+        type=$1
+        shift
+        fi
+  done
+  printf "${export_tb1}---${load_tb1}---${tbs_space} \n"
+else 
+  printf "Usage:$0 -t1 export_tb1 -t2 load_tb1 -s tbs_space -p type \n"
+  exit 1
+fi  
+
+echo "set database parameter.......\n"
+if [ $type == '1' ]; then
+    ExportDbName="YNBIDB"
+    ExportDbUser="aibass"
+    ExportDbPassword="newbi1001"
+    LoadDbName="YNBIYJ"
+    LoadDbUser="biyjinst" 
+    LoadDbPassword="bass1gen_db"
+    echo "---${export_tb1} start to export from bass2node to bass1node---and tablename of bass1node is ${load_tb1}------\n"
+    elif [ $type == '2' ]; then
+    ExportDbName="YNBIYJ"
+    ExportDbUser="biyjinst"
+    ExportDbPassword="bass1gen_db"
+    LoadDbName="YNBIDB"
+    LoadDbUser="aibass"
+    LoadDbPassword="newbi1001"
+    echo "---${export_tb1} start to export from bass1node to bass2node---and tablename of bass2node is ${load_tb1}------\n"
+    elif [ $type == '3' ]; then
+    ExportDbName="YNBIDB"
+    ExportDbUser="aibass"
+    ExportDbPassword="newbi1001"
+    LoadDbName="YNBI203"
+    LoadDbUser="bimart"
+    LoadDbPassword="bimart123"
+    echo "---${export_tb1} start to export from bass2node to datamart---and tablename of datamart is ${load_tb1}------\n"
+    elif [ $type == '4' ]; then
+    ExportDbName="YNBIDB"
+    ExportDbUser="aibass"
+    ExportDbPassword="newbi1001"
+    LoadDbName="WEBDB209"
+    LoadDbUser="biqt"
+    LoadDbPassword="biqt123"
+    echo "---${export_tb1} start to export from bass2node to report---and tablename of report is ${load_tb1}------\n"
+    elif [ $type == '5' ]; then
+    ExportDbName="YNBI203"
+    ExportDbUser="bimart"
+    ExportDbPassword="bimart123"
+    LoadDbName="YNBIDB"
+    LoadDbUser="aibass"
+    LoadDbPassword="newbi1001"
+    echo "---${export_tb1} start to export from datamart to bass2node---and tablename of datamart is ${load_tb1}------\n"
+    elif [ $type == '6' ]; then
+    ExportDbName="YNBIDB"
+    ExportDbUser="aibass"
+    ExportDbPassword="newbi1001"
+    LoadDbName="ynbihis"
+    LoadDbUser="aibass"
+    LoadDbPassword="newbi1001"
+    echo "---${export_tb1} start to export from bass2node to HISDB---and tablename of report is ${load_tb1}------\n"
+    elif [ $type == '7' ]; then
+    ExportDbName="ynbihis"
+    ExportDbUser="aibass"
+    ExportDbPassword="newbi1001"
+    LoadDbName="YNBIDB"
+    LoadDbUser="aibass"
+    LoadDbPassword="newbi1001"
+    elif [ $type == '8' ]; then
+    ExportDbName="ynbihis"
+    ExportDbUser="aibass"
+    ExportDbPassword="newbi1001"
+    LoadDbName="YNBI203"
+    LoadDbUser="bimart"
+    LoadDbPassword="bimart123"
+    elif [ $type == '9' ]; then
+    ExportDbName="WEBDB209"
+    ExportDbUser="biqt"
+    ExportDbPassword="biqt123"
+    LoadDbName="YNBIDB"
+    LoadDbUser="aibass"
+    LoadDbPassword="newbi1001"
+    echo "---${export_tb1} start to export from bass2node to HISDB---and tablename of report is ${load_tb1}------\n"
+fi
+
+ExportDbName=$(echo $ExportDbName|tr 'a-z' 'A-Z')
+ExportDbUser=$(echo $ExportDbUser|tr 'a-z' 'A-Z') 
+LoadDbName=$(echo $LoadDbName|tr 'a-z' 'A-Z')
+LoadDbUser=$(echo $LoadDbUser|tr 'a-z' 'A-Z') 
+echo "ExportDbName is ${ExportDbName}"
+echo "ExportDbUser is ${ExportDbUser}"
+echo "ExportDbPassword is ${ExportDbPassword}"
+echo "LoadDbName is ${LoadDbName}"
+echo "LoadDbUser is ${LoadDbUser}"
+echo "LoadDbPassword is ${LoadDbPassword} \n" 
+
+Export()
+{ 
+	echo "判断目录是否存在，若不存在，则创建........\n"
+	if [ -d /biyjetl/aibassyj/user/xiaojp/exportload ]; then
+	print "/biyjetl/aibassyj/user/xiaojp/exportload is already exist...."
+	else  
+	mkdir /biyjetl/aibassyj/user/xiaojp/exportload 
+	fi
+	db2 "connect to ${ExportDbName} user ${ExportDbUser} using ${ExportDbPassword}"    
+	if [ $? -ne 0 ]
+	then 
+	  printf "connect to ${ExportDbName} fail......\n"
+	  return 1
+  fi
+  echo "${ExportDbName} connect success ........\n" 
+  printf "export to /biyjetl/aibassyj/user/xiaojp/exportload/${export_tb1}.txt of del modified by COLDEL0x01 nochardel decplusblank striplzeros select * from  ${export_tb1}"
+  ## modified by COLDEL0x01 nochardel decplusblank striplzeros
+	db2 "export to /biyjetl/aibassyj/user/xiaojp/exportload/${export_tb1}.txt of del select * from ${export_tb1}"
+	if [ $? -ne 0 ]
+	then 
+	  printf "export table ${export_tb1} fail......"
+	  return 1
+  fi
+  db2 terminate
+}	
+
+source_schema=$(echo $export_tb1|cut -d "." -f 1|tr 'a-z' 'A-Z')
+source_table=$(echo $export_tb1|cut -d "." -f 2|tr 'a-z' 'A-Z')
+target_schema=$(echo $load_tb1|cut -d "." -f 1|tr 'a-z' 'A-Z')
+target_table=$(echo $load_tb1|cut -d "." -f 2|tr 'a-z' 'A-Z')
+tbs_space=$(echo $tbs_space|tr 'a-z' 'A-Z')
+
+trimspace()
+{
+        echo $1|sed "s/^[ ]*//g"|sed "s/[ ]*$//g"
+}
+
+file_deal()
+{
+	db2 "connect to ${ExportDbName} user ${ExportDbUser} using ${ExportDbPassword}"    
+	sqlstmt="select tbsp_name from sysibmADM.TBSP_utilization group by tbsp_name order by tbsp_name desc" 
+	db2 -tx $sqlstmt|while read line
+	do
+	soure_space=`trimspace $line`
+	echo "------grep -i $soure_space ${source_table}.sql-----"
+	flag=`grep -i -l $soure_space ${source_table}.sql` 
+	if [ x$flag == "x" ]; then
+	continue
+	else
+	echo "cat ${source_table}.sql|sed -e "s/\"$soure_space\"/\"$tbs_space\"/g" -e "s/$source_schema/$target_schema/g" -e "s/$source_table/$target_table/g" -e "s/CONNECT TO ${ExportDbName} USER ${ExportDbUser}/CONNECT TO ${LoadDbName} USER ${LoadDbUser} using ${LoadDbPassword}/g" "
+	cat ${source_table}.sql|sed -e "s/\"$soure_space\"/\"$tbs_space\"/g" -e "s/$source_schema/$target_schema/g" -e "s/$source_table/$target_table/g" -e "s/CONNECT TO ${ExportDbName} USER ${ExportDbUser}/CONNECT TO ${LoadDbName} USER ${LoadDbUser} using ${LoadDbPassword}/g" |tee ${source_table}.sql
+	fi 
+	done
+}
+
+Get_Tbstructure()
+ { 
+     db2look -d ${ExportDbName} -z $source_schema -noview -e -I ${ExportDbUser} -W ${ExportDbPassword} -t ${source_table} |tr 'a-z' 'A-Z' |tee ${source_table}.sql 
+     #chmod 755 ${source_table}.sql 
+     file_deal 
+     if [ $? -ne 0 ] 
+     then
+         echo "get tbs_structure of ${source_table} fail...."
+         exit -1
+     fi  
+ }
+
+Create()
+ {
+ 	db2 "connect to ${LoadDbName} user ${LoadDbUser} using ${LoadDbPassword}" 
+ 	if [ $? -ne 0 ]
+ 	then 
+ 	  printf "connect to ${LoadDbName} fail......"
+ 	  return 1
+   fi
+   echo "${LoadDbName} connect success ........\n"
+   db2 "drop table ${load_tb1}"
+   echo "drop table ${load_tb1} success ........\n"
+   printf "create table ${load_tb1}..................." 
+   db2 -tvf ${source_table}.sql 
+   echo $? 
+   if [ $? -ne 0 ] 
+   then
+         printf "create table name :${load_tb1} fail."
+         return 1
+   fi
+      db2 terminate 
+ }	
+
+Load()
+{
+	db2 "connect to ${LoadDbName} user ${LoadDbUser} using ${LoadDbPassword}" 
+	if [ $? -ne 0 ]
+	then 
+	  printf "connect to ${LoadDbName} fail......"
+	  return 1
+  fi
+  echo " ${LoadDbName} conect success.....\n"
+  ##modified by COLDEL0x01 nochardel decplusblank
+  db2 "load client FROM /biyjetl/aibassyj/user/xiaojp/exportload/${export_tb1}.txt of del  replace into ${load_tb1}"
+  if [ $? -ne 0 ]
+     then
+        printf "Load table name :${load_tb1} fail."
+        return 1
+  fi
+     db2 terminate
+}
+echo "strat export ${export_tb1}......"
+Export
+if [ $? -ne 0 ] 
+   then 
+      printf "export table ${export_tb1} fail......\n"
+	    exit 1
+	 else
+	    printf "export table ${export_tb1} success......\n"
+	    echo "get structure of ${export_tb1}......\n" 
+      Get_Tbstructure
+      echo "start Creating ${load_tb1}......\n" 
+      Create
+      if [ $? -ne 0 ] 
+        then
+          printf "create table name :${load_tb1} fail.\n"
+          exit 1
+        else 
+          printf "create table name :${load_tb1} success...\n" 
+          echo "start loading ${load_tb1}......\n" 
+          Load
+          if [ $? -ne 0 ]
+            then
+                printf "Load table name :${load_tb1} fail.\n"
+                exit 1
+            else 
+                printf "Load table name :${load_tb1} success.\n"
+          fi
+      fi
+fi
+   
